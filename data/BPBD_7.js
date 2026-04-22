941f8afe45e4af1d5c8ac4903887330dfc1d20e1
cindywahyuu <cindywahyukarisma@mail.ugm.ac.id>
2026-04-22 16:59:39 +0700

    Dashboard GZone

---
+++ data/BPBD_7.js
@@ -0,0 +1,1 @@
+var json_BPBD_7 = {"type":"FeatureCollection","name":"BPBD_7","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Id":1.0,"Keterangan":null},"geometry":{"type":"Point","coordinates":[110.321857235192553,-7.899501144038693]}}]}
